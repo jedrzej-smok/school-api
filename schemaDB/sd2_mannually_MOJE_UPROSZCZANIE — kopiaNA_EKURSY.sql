74e2919707da1821cfaa46dfbe99b3ef41ef09c5
jedrzejSmok <jedrzejsmok55@gmail.com>
2021-11-25 16:06:17 +0100

    sd4_doOdtworzeniaFinal wersja z adminem i przykladowymi i funckja i procedura

--- schemaDB/sd2_mannually_MOJE_UPROSZCZANIE — kopiaNA_EKURSY.sql
+++ schemaDB/sd2_mannually_MOJE_UPROSZCZANIE — kopiaNA_EKURSY.sql
@@ -26,9 +26,9 @@ SET time_zone = "+00:00";
 --
 -- Struktura tabeli dla tabeli `instruktorzy`
 --
-
+CREATE SEQUENCE seq_id_1 START WITH 10 INCREMENT BY 1;
 CREATE TABLE `instruktorzy` (
-  `idInstruktora` int(11) NOT NULL AUTO_INCREMENT,
+  `idInstruktora` int(11) NOT NULL DEFAULT NEXTVAL(seq_id_1),
   `email` varchar(100) COLLATE utf8_polish_ci NOT NULL,
   `haslo` varchar(100) COLLATE utf8_polish_ci NOT NULL,
   `imie` varchar(50) COLLATE utf8_polish_ci NOT NULL,
@@ -49,9 +49,9 @@ INSERT INTO `instruktorzy` (`idInstruktora`, `email`, `haslo`, `imie`, `nazwisko
 --
 -- Struktura tabeli dla tabeli `gatunki`
 --
-
+CREATE SEQUENCE seq_id_2 START WITH 10 INCREMENT BY 1;
 CREATE TABLE `gatunki` (
-  `idGatunku` int(11) NOT NULL AUTO_INCREMENT,
+  `idGatunku` int(11) NOT NULL DEFAULT NEXTVAL(seq_id_2),
   `nazwa` varchar(50) COLLATE utf8_polish_ci NOT NULL,
    PRIMARY KEY (`idGatunku`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
@@ -73,6 +73,8 @@ CREATE TABLE `instruktorgatunek` (
   `idInstruktora` int(11) NOT NULL,
   `idGatunku` int(11) NOT NULL,
 PRIMARY KEY (`idInstruktora`,`idGatunku`),
+INDEX(`idInstruktora`),
+INDEX(`idGatunku`),
 CONSTRAINT `instruktorgatunek_FK_idInstruktora` FOREIGN KEY (`idInstruktora`) REFERENCES `instruktorzy` (`idInstruktora`) ON DELETE CASCADE ON UPDATE CASCADE,
 CONSTRAINT `instruktorgatunek_FK_idGatunku` FOREIGN KEY (`idGatunku`) REFERENCES `gatunki` (`idGatunku`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
@@ -90,9 +92,9 @@ INSERT INTO `instruktorgatunek` (`idInstruktora`, `idGatunku`) VALUES
 --
 -- Struktura tabeli dla tabeli `poziomy`
 --
-
+CREATE SEQUENCE seq_id_3 START WITH 10 INCREMENT BY 1;
 CREATE TABLE `poziomy` (
-  `idPoziomu` int(11) NOT NULL AUTO_INCREMENT,
+  `idPoziomu` int(11) NOT NULL DEFAULT NEXTVAL(seq_id_3),
   `nazwa` varchar(50) COLLATE utf8_polish_ci NOT NULL,
 PRIMARY KEY (`idPoziomu`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
@@ -102,9 +104,9 @@ PRIMARY KEY (`idPoziomu`)
 --
 -- Struktura tabeli dla tabeli `sale`
 --
-
+CREATE SEQUENCE seq_id_4 START WITH 10 INCREMENT BY 1;
 CREATE TABLE `sale` (
-  `idSali` int(11) NOT NULL AUTO_INCREMENT,
+  `idSali` int(11) NOT NULL DEFAULT NEXTVAL(seq_id_4),
   `nrSali` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `pojemnosc` int(11) NOT NULL,
 PRIMARY KEY (`idSali`)
@@ -115,9 +117,9 @@ PRIMARY KEY (`idSali`)
 --
 -- Struktura tabeli dla tabeli `wykonawcy`
 --
-
+CREATE SEQUENCE seq_id_5 START WITH 10 INCREMENT BY 1;
 CREATE TABLE `wykonawcy` (
-  `idWykonawcy` int(11) NOT NULL AUTO_INCREMENT,
+  `idWykonawcy` int(11) NOT NULL DEFAULT NEXTVAL(seq_id_5),
   `nazwa` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `gatunekMuzyczny` varchar(50) COLLATE utf8_polish_ci DEFAULT NULL,
 PRIMARY KEY (`idWykonawcy`)
@@ -128,14 +130,15 @@ PRIMARY KEY (`idWykonawcy`)
 --
 -- Struktura tabeli dla tabeli `utwory`
 --
-
+CREATE SEQUENCE seq_id_6 START WITH 10 INCREMENT BY 1;
 CREATE TABLE `utwory` (
-  `idUtworu` int(11) NOT NULL AUTO_INCREMENT,
+  `idUtworu` int(11) NOT NULL DEFAULT NEXTVAL(seq_id_6),
   `wykonawca` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `tytul` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `zrodlo` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `idWykonawcy` int(11) NOT NULL,
 PRIMARY KEY (`idUtworu`),
+INDEX(`idWykonawcy`),
 CONSTRAINT `utwory_FK_idWykonawcy` FOREIGN KEY (`idWykonawcy`) REFERENCES `wykonawcy` (`idWykonawcy`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
@@ -144,9 +147,9 @@ CONSTRAINT `utwory_FK_idWykonawcy` FOREIGN KEY (`idWykonawcy`) REFERENCES `wykon
 --
 -- Struktura tabeli dla tabeli `kursy`
 --
-
+CREATE SEQUENCE seq_id_7 START WITH 10 INCREMENT BY 1;
 CREATE TABLE `kursy` (
-  `idKursu` int(11) NOT NULL AUTO_INCREMENT,
+  `idKursu` int(11) NOT NULL DEFAULT NEXTVAL(seq_id_7),
   `nazwa` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `cena` double NOT NULL,
   `iloscZajec` int(11) NOT NULL,
@@ -157,6 +160,10 @@ CREATE TABLE `kursy` (
   `idUtworu` int(11) NOT NULL,
   `idPoziomu` int(11) NOT NULL,
 PRIMARY KEY (`idKursu`),
+INDEX(`idSali`),
+INDEX(`idUtworu`),
+INDEX(`idPoziomu`),
+INDEX(`idGatunku`),
 CONSTRAINT `kursy_FK_idSali` FOREIGN KEY (`idSali`) REFERENCES `sale` (`idSali`) ON DELETE CASCADE ON UPDATE CASCADE,
 CONSTRAINT `kursy_FK_idUtworu` FOREIGN KEY (`idUtworu`) REFERENCES `utwory` (`idUtworu`) ON DELETE CASCADE ON UPDATE CASCADE,
 CONSTRAINT `kursy_FK_idPoziomu`  FOREIGN KEY (`idPoziomu`) REFERENCES `poziomy` (`idPoziomu`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -168,13 +175,14 @@ CONSTRAINT `kursy_FK_idGatunku` FOREIGN KEY (`idGatunku`) REFERENCES `gatunki` (
 --
 -- Struktura tabeli dla tabeli `nagraniaukladow`
 --
-
+CREATE SEQUENCE seq_id_8 START WITH 10 INCREMENT BY 1;
 CREATE TABLE `nagraniaukladow` (
-  `idNagrania` int(11) NOT NULL AUTO_INCREMENT,
+  `idNagrania` int(11) NOT NULL DEFAULT NEXTVAL(seq_id_8),
   `nrZajec` int(11) NOT NULL,
   `zrodlo` varchar(100) COLLATE utf8_polish_ci NOT NULL,
   `idKursu` int(11) NOT NULL,
 PRIMARY KEY (`idNagrania`),
+INDEX(`idKursu`),
 CONSTRAINT `nagraniaukladow_FK_idKursu` FOREIGN KEY (`idKursu`) REFERENCES `kursy` (`idKursu`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
@@ -183,13 +191,15 @@ CONSTRAINT `nagraniaukladow_FK_idKursu` FOREIGN KEY (`idKursu`) REFERENCES `kurs
 --
 -- Struktura tabeli dla tabeli `przypisania`
 --
-
+CREATE SEQUENCE seq_id_9 START WITH 10 INCREMENT BY 1;
 CREATE TABLE `przypisania` (
-  `idPrzypisania` int(11) NOT NULL AUTO_INCREMENT,
+  `idPrzypisania` int(11) NOT NULL DEFAULT NEXTVAL(seq_id_9),
   `wynagrodzenie` double NOT NULL,
   `idKursu` int(11) NOT NULL,
   `idInstruktora` int(11) NOT NULL,
 PRIMARY KEY (`idPrzypisania`),
+INDEX(`idKursu`),
+INDEX(`idInstruktora`),
 CONSTRAINT `przypisania_FK_idKursu` FOREIGN KEY (`idKursu`) REFERENCES `kursy` (`idKursu`) ON DELETE CASCADE ON UPDATE CASCADE,
 CONSTRAINT `przypisania_FK_idInstruktora` FOREIGN KEY (`idInstruktora`) REFERENCES `instruktorzy` (`idInstruktora`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
@@ -199,9 +209,9 @@ CONSTRAINT `przypisania_FK_idInstruktora` FOREIGN KEY (`idInstruktora`) REFERENC
 --
 -- Struktura tabeli dla tabeli `uczestnicy`
 --
-
+CREATE SEQUENCE seq_id_10 START WITH 10 INCREMENT BY 1;
 CREATE TABLE `uczestnicy` (
-  `idUczesntika` int(11) NOT NULL AUTO_INCREMENT,
+  `idUczesntika` int(11) NOT NULL DEFAULT NEXTVAL(seq_id_10),
   `email` varchar(100) COLLATE utf8_polish_ci NOT NULL,
   `haslo` varchar(100) COLLATE utf8_polish_ci NOT NULL,
   `imie` varchar(50) COLLATE utf8_polish_ci NOT NULL,
@@ -214,13 +224,15 @@ PRIMARY KEY (`idUczesntika`)
 --
 -- Struktura tabeli dla tabeli `zapisy`
 --
-
+CREATE SEQUENCE seq_id_11 START WITH 10 INCREMENT BY 1;
 CREATE TABLE `zapisy` (
-  `idZapisu` int(11) NOT NULL AUTO_INCREMENT,
+  `idZapisu` int(11) NOT NULL DEFAULT NEXTVAL(seq_id_11),
   `pozostaleZajecia` int(11) NOT NULL,
   `idUczestnika` int(11) NOT NULL,
   `idKursu` int(11) NOT NULL,
 PRIMARY KEY (`idZapisu`),
+INDEX(`idUczestnika`),
+INDEX(`idKursu`),
 CONSTRAINT `zapisy_FK_idUczestnika` FOREIGN KEY (`idUczestnika`) REFERENCES `uczestnicy` (`idUczesntika`) ON DELETE CASCADE ON UPDATE CASCADE,
 CONSTRAINT `zapisy_FK_IdKursu` FOREIGN KEY (`idKursu`) REFERENCES `kursy` (`idKursu`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
