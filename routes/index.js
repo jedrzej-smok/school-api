5dda9abe5b44537458568a9ba34f39c16fcc706e
jedrzejSmok <jedrzejsmok55@gmail.com>
2022-01-13 16:30:29 +0100

    delete temp file

--- routes/index.js
+++
@@ -1,6 +0,0 @@
-const {comparePassword, hashPassword} = require('../utils/myBcrypt');
-(async () => {
-    const hash = await hashPassword('');
-    console.log(hash);
-    console.log(await(comparePassword('', '$2b$10$WIUSh5Dl990n/zt.cJp88eV2Rie8be5aEPvVwDIuyoMENE0CQt9Aq')));
-})();
